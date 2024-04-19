dfcef7bf1c50ae57825185beb512e42eb1a9c8f9
imtithal <halaloimtithal@gmail.com>
2024-04-20 02:06:22 +0300

    adding menu section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,4 +13,23 @@
         </div>
     </div>
 </div>
+<section class="p-5">
+    <div class="container">
+        <div class="flex flex-wrap justify-between items-center">
+            <div class="w-full md:w-1/2 pr-0 md:pr-5">
+                <div class="heading-section text-right ftco-animate">
+                    <span class="subheading text-gray-700">Discover</span>
+                    <h2 class="mb-4 text-2xl md:text-4xl">Our Menu</h2>
+                    <p class="mb-4 text-gray-700">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                    <p><a href="#" class="btn btn-warning px-4 py-3">View Full Menu</a></p>
+                </div>
+            </div>
+            <div class="w-full md:w-1/2 flex justify-center">
+                <img src="{{ asset('assets/coffee.jpeg') }}" alt="Image Description" class="w-50 h-auto">
+            </div>
+        </div>
+    </div>
+</section>
+
+
 @endsection
