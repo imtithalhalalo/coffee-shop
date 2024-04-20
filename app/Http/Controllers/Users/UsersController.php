8d394e90fd37b33e3396c1d10fb91d9ce24e1650
imtithal <halaloimtithal@gmail.com>
2024-04-20 23:50:32 +0300

    adding display orders and bookings

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product\Booking;
+use App\Models\Product\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
     
     public function displayOrders() {
+        $orders = Order::where("user_id", Auth::id())->orderBy("created_at","desc")->paginate(10);
+
+        return view("users.orders", compact("orders"));
+    }
+
+    public function displayBookings() {
+        $bookings = Booking::where("user_id", Auth::id())->orderBy("created_at","desc")->paginate(10);
+
+        return view("users.bookings", compact("bookings"));
     }
 
 }
