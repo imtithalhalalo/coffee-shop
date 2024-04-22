ff1a90a7a3aaff1320fb9c1cf14e3ab16295994a
imtithal <halaloimtithal@gmail.com>
2024-04-22 03:00:43 +0300

    changing orders of the menu

--- resources/views/products/menu.blade.php
+++ resources/views/products/menu.blade.php
@@ -2,13 +2,47 @@
 
 @section('content')
 
-<section>
-    <div class="container">
-        <div class="row">
+
+<div class="bg-white">
+
+    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div class="px-4 mt-4 sm:px-0">
             <h3 class="text-base font-semibold leading-7 text-gray-900 fs-1">Our Menu</h3>
             <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Please find our menu below.</p>
         </div>
+        <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-4 xl:gap-x-8" id="v-pills-2">
+            @foreach($products as $product)
+            <div class="group relative">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img src="{{ asset('assets/'.$product->image.'') }}" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                <div>
+                    <h3 class="text-sm text-gray-700">
+                    <a href="{{ route('product.details', $product->id) }}">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                        {{ $product->name }}
+                    </a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500">{{ $product->description }}</p>
+                </div>
+                <p class="text-sm font-medium text-gray-900">${{ $product->price }}</p>
+                </div>
+
+                <a href="{{ route('product.details', $product->id) }}" class="btn btn-warning mt-2">Show</a>
+            </div>
+        @endforeach
+    </div>
+
+    
+  </div>
+</div>
+
+
+<section>
+    <div class="container">
+        <div class="row">
+
             <div class="col-md-6">
                 <h3 class="mt-5 fs-2">Desserts</h3>
                 <ul role="list" class="divide-y divide-gray-100">
@@ -52,35 +86,5 @@
         </div>
     </div>
 </section>
-<div class="bg-white">
-  <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Menu</h2>
-    <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-4 xl:gap-x-8" id="v-pills-2">
-        @foreach($products as $product)
-        <div class="group relative">
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-            <img src="{{ asset('assets/'.$product->image.'') }}" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
-            </div>
-            <div class="mt-4 flex justify-between">
-            <div>
-                <h3 class="text-sm text-gray-700">
-                <a href="{{ route('product.details', $product->id) }}">
-                    <span aria-hidden="true" class="absolute inset-0"></span>
-                    {{ $product->name }}
-                </a>
-                </h3>
-                <p class="mt-1 text-sm text-gray-500">{{ $product->description }}</p>
-            </div>
-            <p class="text-sm font-medium text-gray-900">${{ $product->price }}</p>
-            </div>
-
-            <a href="{{ route('product.details', $product->id) }}" class="btn btn-warning mt-2">Show</a>
-        </div>
-      @endforeach
-    </div>
-
-    
-  </div>
-</div>
 
 @endsection
