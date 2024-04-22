3d9b31ff4c4f7ca820258ff06477e3eeccc1ebc5
imtithal <halaloimtithal@gmail.com>
2024-04-23 00:10:41 +0300

    adding type to products

--- database/migrations/2024_04_19_230749_create_products_table.php
+++ database/migrations/2024_04_19_230749_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('image')->nullable();
             $table->string('price'); 
+            $table->string('type'); 
             $table->text('description')->nullable();
             $table->timestamps();
         });
