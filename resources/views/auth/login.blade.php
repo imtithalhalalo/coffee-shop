41c673414684d511c352bd86b4cc87322a685a62
imtithal <halaloimtithal@gmail.com>
2024-04-23 00:11:04 +0300

    adding login with google button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,19 +35,39 @@
                             placeholder="Password"
                         >
                     </div>
-                    <div class="flex justify-center">
+                    <div class="flex justify-content-end ">
+                        @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif
                         <button 
                             type="submit" 
-                            class="w-full px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
-                            Login
+                            class="px-3 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 w-25">
+                            Log In
                         </button>
-                        
                     </div>
-                    @if (Route::has('password.request'))
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('Forgot Your Password?') }}
+
+                    <div class="mb-4 mt-2 text-white text-center">
+                    <div class="border-b border-black w-full my-2"></div>
+
+                        <span class="inline-block mx-2">Or</span>
+                    </div>
+                    <div class="text-center mb-6">
+                        <a href="{{ route('google-auth') }}">
+                            <div class="w-2/3 mx-auto px-4 py-2 bg-[#1f2937] text-white rounded-md hover:bg-[#1f2937cc] focus:outline-none focus:bg-[#1f2937cc]">
+                                <div class="flex flex-row justify-between align-items-center ">
+                                    <div>
+                                    <svg fill='#fff' xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 32 32">
+                                        <path d="M 16.003906 14.0625 L 16.003906 18.265625 L 21.992188 18.265625 C 21.210938 20.8125 19.082031 22.636719 16.003906 22.636719 C 12.339844 22.636719 9.367188 19.664063 9.367188 16 C 9.367188 12.335938 12.335938 9.363281 16.003906 9.363281 C 17.652344 9.363281 19.15625 9.96875 20.316406 10.964844 L 23.410156 7.867188 C 21.457031 6.085938 18.855469 5 16.003906 5 C 9.925781 5 5 9.925781 5 16 C 5 22.074219 9.925781 27 16.003906 27 C 25.238281 27 27.277344 18.363281 26.371094 14.078125 Z"></path>
+                                    </svg>
+                                    </div>
+                                    <div>Continue with Google</div>
+                                </div>
+                            </div>
                         </a>
-                    @endif
+                    </div>
+                    
                 </form>
             </div>
         </div>
