e1c5895191e0cd45da03d638b663d70b4c2a6616
imtithal <halaloimtithal@gmail.com>
2024-04-24 17:57:53 +0300

    cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,47 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Users\GoogleAuthController;
+use App\Http\Controllers\Products\ProductsController;
+use App\Http\Controllers\Users\UsersController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+// Routes accessible without authentication
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/services', [HomeController::class, 'services'])->name('services');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::get('products/menu', [ProductsController::class, 'menu'])->name('products.menu');
 
+// Authentication routes
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
-Route::get('/services', [App\Http\Controllers\HomeController::class, 'services'])->name('services');
-Route::get('/about', [App\Http\Controllers\HomeController::class, 'about'])->name('about');
-Route::get('/contact', [App\Http\Controllers\HomeController::class, 'contact'])->name('contact');
+// Google authentication routes
+Route::get('auth/google', [GoogleAuthController::class, 'redirect'])->name('google-auth');
+Route::get('auth/google/call-back', [GoogleAuthController::class, 'callbackGoogle']);
 
-Route::get('auth/google', [App\Http\Controllers\Users\GoogleAuthController::class, 'redirect'])->name('google-auth');
-Route::get('auth/google/call-back', [App\Http\Controllers\Users\GoogleAuthController::class, 'callbackGoogle']);
+// Protected routes requiring authentication
+Route::group(['middleware' => ['auth:web']], function () {
 
-Route::group(['prefix'=> 'products'], function () {
-    //products
-    Route::get('/product-details/{id}', [App\Http\Controllers\Products\ProductsController::class, 'productDetails'])->name('product.details');
-    Route::post('/product-details/{id}', [App\Http\Controllers\Products\ProductsController::class, 'addToCart'])->name('add.cart');
-    Route::get('/cart', [App\Http\Controllers\Products\ProductsController::class, 'cart'])->name('cart')->middleware('auth:web');
-    Route::get('/cart-delete/{id}', [App\Http\Controllers\Products\ProductsController::class, 'deleteProductFromCart'])->name('cart.product.delete');
+    // Products routes
+    Route::group(['prefix'=> 'products'], function () {
+        Route::get('/product-details/{id}', [ProductsController::class, 'productDetails'])->name('product.details');
+        Route::post('/product-details/{id}', [ProductsController::class, 'addToCart'])->name('add.cart');
+        Route::get('/cart', [ProductsController::class, 'cart'])->name('cart');
+        Route::get('/cart-delete/{id}', [ProductsController::class, 'deleteProductFromCart'])->name('cart.product.delete');
+    
+        Route::post('/prepare-checkout', [ProductsController::class, 'prepareCheckout'])->name('prepare.checkout');
+        Route::get('/checkout', [ProductsController::class, 'checkout'])->name('checkout');
+        Route::post('/checkout', [ProductsController::class, 'storeCheckout'])->name('process.checkout');
+    });
 
+    // Book table route
+    Route::post('/booking', [ProductsController::class, 'bookTable'])->name('booking.tables');
 
-    //checkout
-    Route::post('/prepare-checkout', [App\Http\Controllers\Products\ProductsController::class, 'prepareCheckout'])->name('prepare.checkout');
-    Route::get('/checkout', [App\Http\Controllers\Products\ProductsController::class, 'checkout'])->name('checkout');
-    Route::post('/checkout', [App\Http\Controllers\Products\ProductsController::class, 'storeCheckout'])->name('process.checkout');
-
-    //menu
-    Route::get('/menu', [App\Http\Controllers\Products\ProductsController::class, 'menu'])->name('products.menu');
-
-
+    // User routes
+    Route::group(['prefix'=> 'users'], function () {
+        Route::get('/orders', [UsersController::class, 'displayOrders'])->name('users.orders');
+        Route::get('/bookings', [UsersController::class, 'displayBookings'])->name('users.bookings');
+    });
 });
-
-//booking
-Route::post('/booking', [App\Http\Controllers\Products\ProductsController::class, 'bookTable'])->name('booking.tables');
-
-
-Route::group(['prefix'=> 'users'], function () {
-    //user info
-    Route::get('/orders', [App\Http\Controllers\Users\UsersController::class, 'displayOrders'])->name('users.orders')->middleware('auth:web');
-    Route::get('/bookings', [App\Http\Controllers\Users\UsersController::class, 'displayBookings'])->name('users.bookings')->middleware('auth:web');
-
-});
-
